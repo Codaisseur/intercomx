0a0c2bdadbd1141f5c05886ea565595f9e75f72d
Antonio Parisi <ant.parisi@gmail.com>
2019-12-18 15:57:07 +0000

    Add Tag#deleteResource

--- lib/intercomx/resources/tag.ex
+++ lib/intercomx/resources/tag.ex
@@ -11,4 +11,13 @@ defmodule IntercomX.Tag do
         {:error, reason}
     end
   end
+
+  def deleteRequest(id) when is_bitstring(id) do
+    with {:ok, res} <- delete("/tags/" <> id) do
+      {:ok, res.body}
+    else
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
 end
